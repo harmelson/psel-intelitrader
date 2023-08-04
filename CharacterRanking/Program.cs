9408bac2b7b0ebf77fd2cefbc462b37689824ddf
Gabriel Harmel <gabriel.harmel@hotmail.com>
2023-08-04 18:37:21 -0300

    build: create CaesarCipher and CharacterRanking console projects

---
+++ CharacterRanking/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CharacterRanking
+{
+    class Program
+    {
+        static void Main()
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            string message = helloWorld.GetHelloWorld();
+            Console.WriteLine(message);
+        }
+    }
+
+    public class HelloWorld
+    {
+        public string GetHelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
