6c1e7278f4cda48daa96fb8c7fe68dbf23b0ceaa
Gabriel Harmel <gabriel.harmel@hotmail.com>
2023-08-04 22:36:09 -0300

    feat(CharacterRanking):  add GetFileString class

---
+++ CharacterRanking/GetFileString.cs
@@ -0,0 +1,34 @@
+using System.Text;
+
+namespace CharacterRanking
+{
+    public class GetFileString
+    {
+        private string? InputFilePath { get; set; }
+        public string? InputText { get; set; }
+
+        public GetFileString()
+        {
+            GetText();
+        }
+
+        public void GetText()
+        {
+            try
+            {
+                Console.WriteLine("Digite o caminho do arquivo de texto:");
+                InputFilePath = Console.ReadLine() ?? string.Empty;
+
+                string inputText = File.ReadAllText(InputFilePath, Encoding.UTF8);
+
+                InputText = inputText;
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Erro ao ler o arquivo de entrada.");
+                GetText();
+            }
+        }
+
+    }
+}
